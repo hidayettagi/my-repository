d180a1bbb050ab362f773c4b92435aa9f6d933f5
hidayettagi <36880683+hidayettagi@users.noreply.github.com>
2024-03-25 20:11:36 -0400

    update to index file

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#update the OS
 yum update -y
+
+
+#install Apache
 yum install httpd -y
-yum install 
+
+#copy content to /var/www/html folder
+cd /var/www/html
+FOLDER="https://raw.githubusercontent.com/hidayettagi/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
+
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
